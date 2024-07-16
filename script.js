ce7db7ef6e8b7f39fe9ced5fe82fda5391b0f89f
Shruthi <shruthisiva2001@gmail.comm>
2024-07-17 02:08:48 +0530

    update new files

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('hello world');
+var a='Hi'//var can be updated and redeclared it
+console.log(a);
+console.log(window.a)
+
+//let
+let c='hello' //we can't redeclare but can be updated
+c='init'
+console.log(c)
+
+//const
+const k='var'//can't be redeclared and updated
+
+k='ini'
+console.log(k)
